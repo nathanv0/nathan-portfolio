5c53200e8e6f67ef192940bae9ea5f278b6cbf32
Nathan Vo <115041909+nathanv0@users.noreply.github.com>
2025-12-09 11:28:43 -0600

    fix images issues

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,30 +14,30 @@ const navLinks = [
 ];
 
 const words = [
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "./images/ideas.svg" },
+    { text: "Concepts", imgPath: "./images/concepts.svg" },
+    { text: "Designs", imgPath: "./images/designs.svg" },
+    { text: "Code", imgPath: "./images/code.svg" },
+    { text: "Ideas", imgPath: "./images/ideas.svg" },
+    { text: "Concepts", imgPath: "./images/concepts.svg" },
+    { text: "Designs", imgPath: "./images/designs.svg" },
+    { text: "Code", imgPath: "./images/code.svg" },
 ];
 
 
 const abilities = [
     {
-        imgPath: "/images/seo.png",
+        imgPath: "./images/seo.png",
         title: "Attention to Detail",
         desc: "I prioritize clean, bug-free code and pixel-perfect designs to ensure a polished user experience.",
     },
     {
-        imgPath: "/images/devices.png",
+        imgPath: "./images/devices.png",
         title: "Problem Solver",
         desc: "I thrive on breaking down complex technical challenges and engineering efficient, scalable solutions.",
     },
     {
-        imgPath: "/images/time.png",
+        imgPath: "./images/time.png",
         title: "Adaptability",
         desc: "I quickly master new languages and frameworks (like switching from Java to React) to stay ahead of the curve.",
     },
@@ -46,54 +46,54 @@ const abilities = [
 const techStackImgs = [
     {
         name: "HTML",
-        imgPath: "/images/logos/html.svg",
+        imgPath: "./images/logos/html.svg",
     },
     {
         name: "CSS",
-        imgPath: "/images/logos/css.svg",
+        imgPath: "./images/logos/css.svg",
     },
     {
         name: "JavaScript",
-        imgPath: "/images/logos/javascript.svg",
+        imgPath: "./images/logos/javascript.svg",
     },
     {
         name: "VSCode",
-        imgPath: "/images/logos/vscode.svg",
+        imgPath: "./images/logos/vscode.svg",
     },
     {
         name: "Github",
-        imgPath: "/images/logos/github.svg",
+        imgPath: "./images/logos/github.svg",
     },
 ];
 
 const techStackIcons = [
     {
         name: "React",
-        modelPath: "/models/react_logo-transformed.glb",
+        modelPath: "./models/react_logo-transformed.glb",
         scale: 1,
         rotation: [0, 0, 0],
     },
     {
         name: "Python",
-        modelPath: "/models/python-transformed.glb",
+        modelPath: "./models/python-transformed.glb",
         scale: 0.8,
         rotation: [0, 0, 0],
     },
     {
         name: "Java",
-        modelPath: "/models/java.glb",
+        modelPath: "./models/java.glb",
         scale: 1.5,
         rotation: [0, -Math.PI / 2, 0],
     },
     {
         name: "Three JS",
-        modelPath: "/models/three.js-transformed.glb",
+        modelPath: "./models/three.js-transformed.glb",
         scale: 0.05,
         rotation: [0, 0, 0],
     },
     {
         name: "Git",
-        modelPath: "/models/git-svg-transformed.glb",
+        modelPath: "./models/git-svg-transformed.glb",
         scale: 0.05,
         rotation: [0, -Math.PI / 4, 0],
     },
@@ -101,7 +101,7 @@ const techStackIcons = [
 
 const expCards = [
     {
-        imgPath: "/images/stem.png",
+        imgPath: "./images/stem.png",
         title: "Software Engineer Technical Coach (Volunteer)",
         date: "November 2024 - May 2025",
         responsibilities: [
@@ -110,7 +110,7 @@ const expCards = [
         ],
     },
     {
-        imgPath: "/images/biomerics.jpg",
+        imgPath: "./images/biomerics.jpg",
         title: "Production Operator Specialist",
         date: "February 2024 - August 2024",
         responsibilities: [
@@ -121,7 +121,7 @@ const expCards = [
     },
     {
 
-        imgPath: "/images/kyoto.jpg",
+        imgPath: "./images/kyoto.jpg",
         title: "Sushi Server",
         date: "October 2022 - February 2024",
         responsibilities: [
@@ -136,17 +136,17 @@ const socialImgs = [
     {
         name: "insta",
         url: "https://www.instagram.com/",
-        imgPath: "/images/insta.png",
+        imgPath: "./images/insta.png",
     },
     {
         name: "fb",
         url: "https://www.facebook.com/",
-        imgPath: "/images/fb.png",
+        imgPath: "./images/fb.png",
     },
     {
         name: "linkedin",
         url: "https://www.linkedin.com/in/nathan-v0/",
-        imgPath: "/images/linkedin.png",
+        imgPath: "./images/linkedin.png",
     },
 ];
 
